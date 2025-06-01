282ec91de68d70edf9f74a92f529fbc14dafd50f
Akash Madavi <akashmadavi2001@gmail.com>
2025-06-01 07:28:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+new HeaderStyle({
+      "count": 10,
+      "size": {
+        "min": 1300,
+        "max": 1500,
+        "pulse": 0
+      },
+      "speed": {
+        "x": {
+          "min": 0.1,
+          "max": 0.6
+        },
+        "y": {
+          "min": 0.1,
+          "max": 0.6
+        }
+      },
+      "colors": {
+        "background": "#000000",
+        "particles": [
+          "#051923",
+          "#003554",
+          "#006494",
+          "#0582ca",
+          "#00a6fb"
+        ]
+      },
+      "blending": "overlay",
+      "opacity": {
+        "center": 0.5,
+        "edge": 0.05
+      },
+      "skew": 0,
+      "shapes": [
+        "c"
+      ]
+    });
